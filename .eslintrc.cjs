39d3dbf20c0faf942acd1afb97f2b253b3570de9
2wndrhs <2wndrhs@gmail.com>
2023-05-25 20:45:08 +0900

    chore: `eslint`, `prettier` 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,23 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
+    'airbnb',
+    'airbnb/hooks',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
   },
-}
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+};
