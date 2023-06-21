6e4e200c90a86dd633ec8edfe0715f29aa3dd1b1
yoo-jimin127 <dbwlals9936@naver.com>
2023-06-21 20:02:55 +0900

    feat: login/register Link 연결

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,15 @@
+import { Link, useNavigate } from "react-router-dom";
+
 function Login() {
+    const navigate = useNavigate();
+
     return <div className="auth-page">
         <div className="container page">
             <div className="row">
                 <div className="col-md-6 offset-md-3 col-xs-12">
-                    <h1 className="text-xs-center">Sign up</h1>
+                    <h1 className="text-xs-center">Sign in</h1>
                     <p className="text-xs-center">
-                        <a href="">Have an account?</a>
+                        <Link to="/">Need an account?</Link>
                     </p>
 
                     <ul className="error-messages">
@@ -19,7 +23,12 @@ function Login() {
                         <fieldset className="form-group">
                             <input className="form-control form-control-lg" type="password" placeholder="Password"/>
                         </fieldset>
-                        <button className="btn btn-lg btn-primary pull-xs-right">Sign up</button>
+                        <button 
+                            type="button"
+                            onClick={() => {navigate("/")}}
+                            className="btn btn-lg btn-primary pull-xs-right">
+                            Sign in
+                        </button>
                     </form>
                 </div>
             </div>
