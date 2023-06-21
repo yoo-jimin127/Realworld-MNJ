827ed748e175e8fbed8ec9aa276e2c84b5ca8c9c
2wndrhs <2wndrhs@gmail.com>
2023-06-21 19:43:23 +0900

    feat: `Header` router 연결

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,27 +1,46 @@
+import { Link } from 'react-router-dom';
+
 function Header() {
   return (
-  <nav className="navbar navbar-light">
-    <div className="container">
-      <a className="navbar-brand" href="index.html">conduit</a>
-      <ul className="nav navbar-nav pull-xs-right">
-        <li className="nav-item">
-          <a className="nav-link active" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/"> <i className="ion-compose"/>&nbsp;New Article </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/"> <i className="ion-gear-a"/>&nbsp;Settings </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">Sign in</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">Sign up</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+    <nav className="navbar navbar-light">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          conduit
+        </Link>
+
+        <ul className="nav navbar-nav pull-xs-right">
+          <li className="nav-item">
+            <Link className="nav-link active" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/editor">
+              {' '}
+              <i className="ion-compose" />
+              &nbsp;New Article{' '}
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/settings">
+              {' '}
+              <i className="ion-gear-a" />
+              &nbsp;Settings{' '}
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/login">
+              Sign in
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/register">
+              Sign up
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
