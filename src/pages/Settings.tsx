aedfe464ed0c7fe62fc7c7bf5d62fc26c6c00b16
yoo-jimin127 <dbwlals9936@naver.com>
2023-07-02 20:46:37 +0900

    feat: `settings` 기능 구현

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,40 +1,93 @@
-function Settings() {
-    return <div className="settings-page">
-        <div className="container page">
-            <div className="row">
-                <div className="col-md-6 offset-md-3 col-xs-12">
-                    <h1 className="text-xs-center">Your Settings</h1>
+import { ChangeEvent, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { useNavigate } from 'react-router-dom';
+import { updateUserInfo } from '../apis';
+import { userState } from '../atoms';
 
-                    <form>
-                        <fieldset>
-                            <fieldset className="form-group">
-                                <input className="form-control" type="text" placeholder="URL of profile picture"/>
-                            </fieldset>
-                            <fieldset className="form-group">
-                                <input className="form-control form-control-lg" type="text" placeholder="Your Name"/>
-                            </fieldset>
-                            <fieldset className="form-group">
-              <textarea
-                  className="form-control form-control-lg"
-                  rows={8}
-                  placeholder="Short bio about you"
-              />
-                            </fieldset>
-                            <fieldset className="form-group">
-                                <input className="form-control form-control-lg" type="text" placeholder="Email"/>
-                            </fieldset>
-                            <fieldset className="form-group">
-                                <input className="form-control form-control-lg" type="password" placeholder="Password"/>
-                            </fieldset>
-                            <button className="btn btn-lg btn-primary pull-xs-right" type="button">Update Settings</button>
-                        </fieldset>
-                    </form>
-                    <hr/>
-                    <button className="btn btn-outline-danger">Or click here to logout.</button>
-                </div>
-            </div>
+function Settings() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [bio, setBio] = useState('');
+  const [image, setImage] = useState('');
+  const [userInfo, setUserInfo] = useRecoilState(userState);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event: ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const { user } = await updateUserInfo({ email, password, username, bio, image });
+    setUserInfo(user);
+    navigate(`/@${username}`);
+  };
+
+  return (
+    <div className="settings-page">
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 col-xs-12">
+            <h1 className="text-xs-center">Your Settings</h1>
+
+            <form onSubmit={handleSubmit}>
+              <fieldset>
+                <fieldset className="form-group">
+                  <input
+                    className="form-control"
+                    type="text"
+                    placeholder="URL of profile picture"
+                    value={image}
+                    onChange={(event) => setImage(event.target.value)}
+                  />
+                </fieldset>
+                <fieldset className="form-group">
+                  <input
+                    className="form-control form-control-lg"
+                    type="text"
+                    placeholder="Your Name"
+                    value={username}
+                    onChange={(event) => setUsername(event.target.value)}
+                  />
+                </fieldset>
+                <fieldset className="form-group">
+                  <textarea
+                    className="form-control form-control-lg"
+                    rows={8}
+                    placeholder="Short bio about you"
+                    value={bio}
+                    onChange={(event) => setBio(event.target.value)}
+                  />
+                </fieldset>
+                <fieldset className="form-group">
+                  <input
+                    className="form-control form-control-lg"
+                    type="text"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                  />
+                </fieldset>
+                <fieldset className="form-group">
+                  <input
+                    className="form-control form-control-lg"
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                  />
+                </fieldset>
+                <button className="btn btn-lg btn-primary pull-xs-right" type="submit">
+                  Update Settings
+                </button>
+              </fieldset>
+            </form>
+            <hr />
+            <button type="submit" className="btn btn-outline-danger">
+              Or click here to logout.
+            </button>
+          </div>
         </div>
-    </div>;
+      </div>
+    </div>
+  );
 }
 
 export default Settings;
