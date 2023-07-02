d136aa0dc44a7a5699495c185dddfea5956d911e
yoo-jimin127 <dbwlals9936@naver.com>
2023-07-02 20:45:49 +0900

    feat: user info update interface 정의

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
-import { RegisterProps, LoginProps } from './types';
+import { RegisterProps, LoginProps, SettingProps } from './types';
 
 const baseURL = `https://api.realworld.io/api`;
 
-// TODO : api 문서 연결
 export const postRegister = async ({ username, email, password }: RegisterProps) => {
   const res = await axios.post(`${baseURL}/users`, { user: { username, email, password } });
   return res.data;
@@ -13,3 +12,8 @@ export const postLogin = async ({ email, password }: LoginProps) => {
   const res = await axios.post(`${baseURL}/users/login`, { user: { email, password } });
   return res.data;
 };
+
+export const updateUserInfo = async ({ email, password, username, bio, image }: SettingProps) => {
+  const res = await axios.put(`${baseURL}/user`, { user: { email, password, username, bio, image }});
+  return res.data;
+}
--- src/apis/types.ts
+++ src/apis/types.ts
@@ -1,9 +1,16 @@
-export type RegisterProps = {
+export interface RegisterProps {
   username: string;
   email: string;
   password: string;
 };
-export type LoginProps = {
+export interface LoginProps {
   email: string;
   password: string;
 };
+export interface SettingProps {
+  email: string;
+  password: string;
+  username: string;
+  bio: string;
+  image: string;
+}
