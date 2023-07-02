40d747fe792bb179bf540c7721ab5fc17dbc1013
2wndrhs <2wndrhs@gmail.com>
2023-07-02 21:59:00 +0900

    feat: `profile article` API 추가

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { RegisterProps, LoginProps, SettingProps } from './types';
 
 const baseURL = `https://api.realworld.io/api`;
-
 const token = localStorage.getItem('token');
+
 const authHttp = axios.create({
   baseURL,
   headers: {
@@ -11,19 +11,45 @@ const authHttp = axios.create({
   },
 });
 
+const http = axios.create({
+  baseURL,
+});
+
 export const postRegister = async ({ username, email, password }: RegisterProps) => {
-  const res = await axios.post(`${baseURL}/users`, { user: { username, email, password } });
+  const res = await http.post(`$/users`, { user: { username, email, password } });
   return res.data;
 };
 
 export const postLogin = async ({ email, password }: LoginProps) => {
-  const res = await axios.post(`${baseURL}/users/login`, { user: { email, password } });
+  const res = await http.post(`/users/login`, { user: { email, password } });
   return res.data;
 };
 
 export const updateUserInfo = async ({ email, password, username, bio, image }: SettingProps) => {
   const res = await authHttp.put('/user', {
     user: { email, password, username, bio, image },
+  });
+  return res.data;
+};
+
+export const getMyArticles = async (username: string) => {
+  const res = await http.get('/articles', {
+    params: {
+      author: username,
+      limit: 5,
+      offset: 0,
+    },
+  });
+  return res.data;
+};
+
+export const getFavoritedArticles = async (username: string) => {
+  const res = await http.get('/articles', {
+    params: {
+      favorited: username,
+      limit: 5,
+      offset: 0,
+    },
   });
   return res.data;
 };
