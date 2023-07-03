81cec717cc28ba9eb80bceff1a2220f4e3e45eb2
2wndrhs <2wndrhs@gmail.com>
2023-07-03 23:52:30 +0900

    refactor: `loader`로 `Article`을 받아오도록 리팩터링

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,81 +1,79 @@
+import { Link, NavLink, LoaderFunctionArgs, useLoaderData } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { userState } from '../atoms';
+import ArticleForm from '../components/profile/ArticleForm';
+import { getFavoritedArticles, getMyArticles } from '../apis';
+import { ArticleResponse } from '../apis/types';
+
+export const myArticlesLoader = async ({ params }: LoaderFunctionArgs) => {
+  const username = params.username!.slice(1);
+  const data = await getMyArticles(username);
+  return data;
+};
+
+export const favoritedArticlesLoader = async ({ params }: LoaderFunctionArgs) => {
+  const username = params.username!.slice(1);
+  const data = await getFavoritedArticles(username);
+  return data;
+};
+
 function Profile() {
-    return <div className="profile-page">
-        <div className="user-info">
-            <div className="container">
-                <div className="row">
-                    <div className="col-xs-12 col-md-10 offset-md-1">
-                        <img src="http://i.imgur.com/Qr71crq.jpg" className="user-img"/>
-                        <h4>Eric Simons</h4>
-                        <p>
-                            Cofounder @GoThinkster, lived in Aol's HQ for a few months, kinda looks like Peeta from
-                            the Hunger Games
-                        </p>
-                        <button className="btn btn-sm btn-outline-secondary action-btn">
-                            <i className="ion-plus-round"></i>
-                            &nbsp; Follow Eric Simons
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
+  const userInfo = useRecoilValue(userState);
+  const { articles } = useLoaderData() as ArticleResponse;
+  console.log('articles', articles);
 
+  return (
+    <div className="profile-page">
+      <div className="user-info">
         <div className="container">
-            <div className="row">
-                <div className="col-xs-12 col-md-10 offset-md-1">
-                    <div className="articles-toggle">
-                        <ul className="nav nav-pills outline-active">
-                            <li className="nav-item">
-                                <a className="nav-link active" href="">My Articles</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="">Favorited Articles</a>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div className="article-preview">
-                        <div className="article-meta">
-                            <a href=""><img src="http://i.imgur.com/Qr71crq.jpg"/></a>
-                            <div className="info">
-                                <a href="" className="author">Eric Simons</a>
-                                <span className="date">January 20th</span>
-                            </div>
-                            <button className="btn btn-outline-primary btn-sm pull-xs-right">
-                                <i className="ion-heart"></i> 29
-                            </button>
-                        </div>
-                        <a href="" className="preview-link">
-                            <h1>How to build webapps that scale</h1>
-                            <p>This is the description for the post.</p>
-                            <span>Read more...</span>
-                        </a>
-                    </div>
-
-                    <div className="article-preview">
-                        <div className="article-meta">
-                            <a href=""><img src="http://i.imgur.com/N4VcUeJ.jpg"/></a>
-                            <div className="info">
-                                <a href="" className="author">Albert Pai</a>
-                                <span className="date">January 20th</span>
-                            </div>
-                            <button className="btn btn-outline-primary btn-sm pull-xs-right">
-                                <i className="ion-heart"></i> 32
-                            </button>
-                        </div>
-                        <a href="" className="preview-link">
-                            <h1>The song you won't ever stop singing. No matter how hard you try.</h1>
-                            <p>This is the description for the post.</p>
-                            <span>Read more...</span>
-                            <ul className="tag-list">
-                                <li className="tag-default tag-pill tag-outline">Music</li>
-                                <li className="tag-default tag-pill tag-outline">Song</li>
-                            </ul>
-                        </a>
-                    </div>
-                </div>
+          <div className="row">
+            <div className="col-xs-12 col-md-10 offset-md-1">
+              <img src="http://i.imgur.com/Qr71crq.jpg" className="user-img" alt="user-img" />
+              <h4>{userInfo.username}</h4>
+              <p>{userInfo.bio}</p>
+              <Link to="/settings" className="btn btn-sm btn-outline-secondary action-btn">
+                <i className="ion-gear-a" />
+                &nbsp; Edit Profile Settings
+              </Link>
             </div>
+          </div>
         </div>
-    </div>;
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12 col-md-10 offset-md-1">
+            <div className="articles-toggle">
+              <ul className="nav nav-pills outline-active">
+                <li className="nav-item">
+                  <NavLink
+                    to={`/@${userInfo.username}`}
+                    className={({ isActive }) => (isActive ? 'active nav-link' : 'nav-link')}
+                    end
+                  >
+                    My Articles
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    to={`/@${userInfo.username}/favorites`}
+                    className={({ isActive }) => (isActive ? 'active nav-link' : 'nav-link')}
+                    end
+                  >
+                    Favorited Articles
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+            {articles.length > 0
+              ? articles.map((article) => <ArticleForm />)
+              : // TODO : css 수정
+                'No articles are here... yet.'}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
