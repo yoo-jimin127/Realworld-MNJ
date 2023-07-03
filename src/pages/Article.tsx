8520e9c4800715c4fc18fda0799b138644a33885
yoo-jimin127 <dbwlals9936@naver.com>
2023-07-04 00:59:25 +0900

    feat: `edit-article` 기능 추가

--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -1,4 +1,4 @@
-import { LoaderFunctionArgs, useLoaderData } from 'react-router-dom';
+import { Link, LoaderFunctionArgs, useLoaderData } from 'react-router-dom';
 import { getArticle } from '../apis';
 import { ArticleListProps } from '../apis/types';
 
@@ -28,15 +28,15 @@ function Article() {
               </a>
               <span className="date">January 20th</span>
             </div>
-            <button className="btn btn-sm btn-outline-secondary" type="button">
-              <i className="ion-plus-round" />
-              &nbsp; Follow {article.author.username} <span className="counter">({})</span>
-            </button>
+            <Link className="btn btn-sm btn-outline-secondary" to={`/editor/${article.slug}`}>
+              <i className="ion-edit" />
+              &nbsp; Edit Article
+            </Link>
             &nbsp;&nbsp;
-            <button className="btn btn-sm btn-outline-primary" type="button">
-              <i className="ion-heart" />
-              &nbsp; Favorite Article <span className="counter">({article.favoritesCount})</span>
-            </button>
+            <Link className="btn btn-sm btn-outline-danger" to={`/editor/${article.slug}`}>
+              <i className="ion-trash-a" />
+              &nbsp; Delete Article
+            </Link>
           </div>
         </div>
       </div>
@@ -81,20 +81,20 @@ function Article() {
         <div className="article-actions">
           <div className="article-meta">
             <a href="profile.html">
-              <img src="http://i.imgur.com/Qr71crq.jpg" />
+              <img alt="" src="http://i.imgur.com/Qr71crq.jpg" />
             </a>
             <div className="info">
-              <a href="" className="author">
+              <a href="/" className="author">
                 Eric Simons
               </a>
               <span className="date">January 20th</span>
             </div>
-            <button className="btn btn-sm btn-outline-secondary">
+            <button type="button" className="btn btn-sm btn-outline-secondary">
               <i className="ion-plus-round" />
               &nbsp; Follow Eric Simons
             </button>
             &nbsp;
-            <button className="btn btn-sm btn-outline-primary">
+            <button type="button" className="btn btn-sm btn-outline-primary">
               <i className="ion-heart" />
               &nbsp; Favorite Post <span className="counter">(29)</span>
             </button>
@@ -105,11 +105,11 @@ function Article() {
           <div className="col-xs-12 col-md-8 offset-md-2">
             <form className="card comment-form">
               <div className="card-block">
-                <textarea className="form-control" placeholder="Write a comment..." rows="3" />
+                <textarea className="form-control" placeholder="Write a comment..." />
               </div>
               <div className="card-footer">
-                <img src="http://i.imgur.com/Qr71crq.jpg" className="comment-author-img" />
-                <button className="btn btn-sm btn-primary">Post Comment</button>
+                <img alt="" src="http://i.imgur.com/Qr71crq.jpg" className="comment-author-img" />
+                <button type="button" className="btn btn-sm btn-primary">Post Comment</button>
               </div>
             </form>
 
@@ -120,11 +120,11 @@ function Article() {
                 </p>
               </div>
               <div className="card-footer">
-                <a href="" className="comment-author">
-                  <img src="http://i.imgur.com/Qr71crq.jpg" className="comment-author-img" />
+                <a href="/" className="comment-author">
+                  <img alt="" src="http://i.imgur.com/Qr71crq.jpg" className="comment-author-img" />
                 </a>
                 &nbsp;
-                <a href="" className="comment-author">
+                <a href="/" className="comment-author">
                   Jacob Schmidt
                 </a>
                 <span className="date-posted">Dec 29th</span>
@@ -138,11 +138,11 @@ function Article() {
                 </p>
               </div>
               <div className="card-footer">
-                <a href="" className="comment-author">
-                  <img src="http://i.imgur.com/Qr71crq.jpg" className="comment-author-img" />
+                <a href="/" className="comment-author">
+                  <img alt="" src="http://i.imgur.com/Qr71crq.jpg" className="comment-author-img" />
                 </a>
                 &nbsp;
-                <a href="" className="comment-author">
+                <a href="/" className="comment-author">
                   Jacob Schmidt
                 </a>
                 <span className="date-posted">Dec 29th</span>
--- src/pages/EditArticle.tsx
+++ src/pages/EditArticle.tsx
@@ -1,37 +1,75 @@
+import React, { useState } from 'react';
+import { LoaderFunctionArgs, useLoaderData, useNavigate } from 'react-router-dom';
+import { getArticle, updateArticle } from '../apis';
+import { ArticleListProps } from '../apis/types';
+
+export const updateArticleLoader = async ({ params }: LoaderFunctionArgs) => {
+  const slug = params.articleSlug!;
+  const { article } = await getArticle(slug);
+  return article;
+};
+
 function EditArticle() {
-    return <div className="editor-page">
-        <div className="container page">
-            <div className="row">
-                <div className="col-md-10 offset-md-1 col-xs-12">
-                    <form>
-                        <fieldset>
-                            <fieldset className="form-group">
-                                <input type="text" className="form-control form-control-lg"
-                                       placeholder="Article Title"/>
-                            </fieldset>
-                            <fieldset className="form-group">
-                                <input type="text" className="form-control" placeholder="What's this article about?"/>
-                            </fieldset>
-                            <fieldset className="form-group">
-              <textarea
-                  className="form-control"
-                  rows={8}
-                  placeholder="Write your article (in markdown)"
-              />
-                            </fieldset>
-                            <fieldset className="form-group">
-                                <input type="text" className="form-control" placeholder="Enter tags"/>
-                                <div className="tag-list"/>
-                            </fieldset>
-                            <button className="btn btn-lg pull-xs-right btn-primary" type="button">
-                                Publish Article
-                            </button>
-                        </fieldset>
-                    </form>
-                </div>
-            </div>
+  const article = useLoaderData() as ArticleListProps;
+  const [title, setTitle] = useState(article.title);
+  const [description, setDescription] = useState(article.description);
+  const [body, setBody] = useState(article.body);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    await updateArticle({ title, description, body }, article.slug);
+    navigate(`/article/${article.slug}`);
+  };
+
+  return (
+    <div className="editor-page">
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-10 offset-md-1 col-xs-12">
+            <form onSubmit={handleSubmit}>
+              <fieldset>
+                <fieldset className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Article Title"
+                    value={title}
+                    onChange={(event) => setTitle(event.target.value)}
+                  />
+                </fieldset>
+                <fieldset className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="What's this article about?"
+                    value={description}
+                    onChange={(event) => setDescription(event.target.value)}
+                  />
+                </fieldset>
+                <fieldset className="form-group">
+                  <textarea
+                    className="form-control"
+                    rows={8}
+                    placeholder="Write your article (in markdown)"
+                    value={body}
+                    onChange={(event) => setBody(event.target.value)}
+                  />
+                </fieldset>
+                <fieldset className="form-group">
+                  <input type="text" className="form-control" placeholder="Enter tags" />
+                  <div className="tag-list" />
+                </fieldset>
+                <button className="btn btn-lg pull-xs-right btn-primary" type="submit">
+                  Publish Article
+                </button>
+              </fieldset>
+            </form>
+          </div>
         </div>
-    </div>;
+      </div>
+    </div>
+  );
 }
 
 export default EditArticle;
