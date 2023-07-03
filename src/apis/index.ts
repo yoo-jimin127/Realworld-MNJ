bd112f0faa38daab1d57b7c2b78ef37ad06c17a3
yoo-jimin127 <dbwlals9936@naver.com>
2023-07-04 00:24:39 +0900

    fix: `interface` type import 추가

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { RegisterProps, LoginProps, SettingProps } from './types';
+import { RegisterProps, LoginProps, SettingProps, ArticleProps } from './types';
 
 const baseURL = `https://api.realworld.io/api`;
 
